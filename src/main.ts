307018974b3e0f533b5398af849eccc4ae7846e9
sarankumar1998 <saranneumann13@gmail.com>
2025-08-12 11:31:34 +0530

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'Cache-Control',
+    ],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,17 +28,25 @@ async function bootstrap() {
     }),
   );
 
+  app.use(
+    express.json({
+      verify: (req: any, _res, buf) => {
+        req.rawBody = buf; 
+      },
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('My API')
     .setDescription('API documentation')
     .setVersion('1.0')
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('v1/documentation', app, document);
 
   await app.listen(4000);
-  console.log(`Application is running`);
+  console.log(`Application is running on port 4000`);
 }
 bootstrap();
